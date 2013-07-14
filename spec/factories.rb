46a781321a73ca187bf69ea27edd0f571567093a
Rodger Yonley <ryonley@gmail.com>
2013-07-14 00:27:13 -0500

    Working on update functionality

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory:user do
-    name    "Michael Hartl"
-    email   "michael@example.com"
-    password  "foobar"
-    password_confirmation "foobar"
+    name    "Rodger"
+    email   "ryonley@gmail.com"
+    password  "tacoma"
+    password_confirmation "tacoma"
   end
 end
